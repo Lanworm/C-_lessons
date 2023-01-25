eae9cbe200678e357067a5b40da85fcee4ece3ab
anton.goloschapov <lanworm@mail.ru>
2023-01-25 08:07:16 +0300

    example_02

---
+++ Example_02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя");
+string usernsme = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(usernsme);
